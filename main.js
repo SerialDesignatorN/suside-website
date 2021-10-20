c48d4a50761d0900b798768d6517f18eb17388b9
zeankun.dev <58360911+zeankundev@users.noreply.github.com>
2021-10-20 07:34:34 +0700

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ if (navigator.userAgent.indexOf("Win") != -1) {
 else if (navigator.userAgent.indexOf("Mac") != -1) {
     platform = "mac";
     console.log("detected macos");
-    btn_download.innerHTML = '<i class="fas fa-download"></i> Mac is unsupported yet! Check back soon!';
+    btn_download.innerHTML = '<i class="fas fa-download"></i> Download for Mac';
 }
 else if (navigator.userAgent.indexOf("Linux") != -1) {
     platform = "linux";
@@ -35,7 +35,8 @@ btn_download.onclick = function() {
         window.location.href = "https://github.com/zeankundev/suside/releases/download/2.3.2/Suside.Editor_Installer_2.3.2_linux_x86_64.AppImage"
     }
     else if (platform == "mac") {
-        window.location.href = "https://bit.ly/2VHhZ4I"
+        window.open('https://bit.ly/2VHhZ4l', '_blank')
+        btn_download.innerHTML = '<i class="far fa-grin-squirt"></i> BAHAHAHA! U HAVE BEEN RICKROLLED!!! ULULULLULULULULU!!!!!'
     }
     else if (platform == "android") {
         window.location.href = "https://bit.ly/2VHhZ4I"
